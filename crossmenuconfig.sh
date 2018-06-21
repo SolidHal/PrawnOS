bcca08a131fc3dfee15f436720c6d42d9e98c5ce
SolidHal <SolidHal@users.noreply.github.com>
2018-06-21 11:33:30 -0500

    Moving from https://github.com/SolidHal/devsus/tree/hybrid_debian

---
+++ crossmenuconfig.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -xe
+
+#Runs Make menuconfig with the proper enviroment vars for cross compiling arm 
+#Grabs the file named config in the same directory as this script, and updates it
+
+KVER=4.9.30
+
+[ ! -f linux-libre-$KVER-gnu.tar.lz ] && wget https://www.linux-libre.fsfla.org/pub/linux-libre/releases/$KVER-gnu/linux-libre-$KVER-gnu.tar.lz
+[ ! -d linux-$KVER ] && tar --lzip -xvf linux-libre-$KVER-gnu.tar.lz
+cd linux-$KVER
+cp ../config .config
+make menuconfig ARCH=arm CROSS_COMPILE=arm-none-eabi- .config
+cp .config ../config
