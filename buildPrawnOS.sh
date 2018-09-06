c254e9e6b163ca904e1e17da292afe6128eb055b
SolidHal <SolidHal@users.noreply.github.com>
2018-09-06 13:40:57 +0000

    Make the kernel get built

--- buildPrawnOS.sh
+++ buildPrawnOS.sh
@@ -7,6 +7,6 @@ sudo echo "Thanks."
 #Build kernel WITHOUT SUDO, building the kernel with sudo is bad practice
 #Once, sometime in the kernel version 2 days building the kernel with sudo broke the
 #The system it was built on
-#./scripts/buildKernel.sh
+./scripts/buildKernel.sh
 #build os WITH sudo, debootstrap requires sudo
 sudo ./scripts/buildDebianFs.sh
