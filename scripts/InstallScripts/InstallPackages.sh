d9f4251eeba3082e2a8f956a7d96d39d77f43c14
SolidHal <solidhal@users.noreply.github.com>
2018-09-06 18:55:46 -0500

    Fixup install scripts, begin move to make build system

--- scripts/InstallScripts/InstallPackages.sh
+++ scripts/InstallScripts/InstallPackages.sh
@@ -2,17 +2,12 @@
 
 DIR=/InstallResources
 
-#Setup locales, crda
-apt install -y locales
-dpkg-reconfigure locales
-apt install -y crda
 locale-gen
 #Install lxqt, wicd
 apt install -y xorg acpi-support lightdm tasksel dpkg librsvg2-common xorg xserver-xorg-input-libinput alsa-utils anacron avahi-daemon eject iw libnss-mdns xdg-utils lxqt wicd-daemon wicd wicd-curses wicd-gtk xserver-xorg-input-synaptics
 #Copy in acpi, pulse audio, trackpad settings, funtion key settings
 cp -rf $DIR/default.pa /etc/pulse/default.pa
 cp -rf $DIR/sound.sh /etc/acpi/sound.sh
-mkdir /etc/acpi/events
 cp -rf $DIR/headphone-acpi-toggle /etc/acpi/events/headphone-acpi-toggle
 mkdir /etc/X11/xorg.conf.d/
 cp -rf $DIR/50-synaptics.conf /etc/X11/xorg.conf.d/
--- scripts/InstallScripts/InstallToInternal.sh
+++ scripts/InstallScripts/InstallToInternal.sh
@@ -14,12 +14,10 @@ then
     sfdisk /dev/mmcblk2 < $RESOURCES/mmc.partmap
     echo Writing kernel partition
     dd if=/dev/sda1 of=/dev/mmcblk2p1
-    echo Writing Filesystem, this will take a moment...
-    #Set the post install script to run after reboot
-    ./$RESOURCES/runonce.sh $RESOURCES/InstallPackages.sh
+    echo Writing Filesystem, this will take about 4 minutes...
     dd if=/dev/sda2 of=/dev/mmcblk2p2 bs=50M
     echo Expanding Filesystem
-    e2fsck -p /dev/mmcblk2p2
+    e2fsck -p -f /dev/mmcblk2p2
     resize2fs /dev/mmcblk2p2
     echo Rebooting... Please remove the usb drive once shutdown is complete
     reboot
