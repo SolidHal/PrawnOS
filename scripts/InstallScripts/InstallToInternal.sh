9d25cac20a5a8fb4308d9572870f47877c08b506
SolidHal <hal@halemmerich.com>
2018-11-04 18:11:20 -0600

    Set reasonal fstab default

--- scripts/InstallScripts/InstallToInternal.sh
+++ scripts/InstallScripts/InstallToInternal.sh
@@ -36,6 +36,8 @@ then
     mkfs.ext4 -F -b 1024 -m 0 -O ^has_journal /dev/mmcblk2p2
     mkdir -p /mnt/mmc/
     rsync -ah --info=progress2 --info=name0 --numeric-ids -x / /mnt/mmc/
+    #Install a base fstab
+    echo "/dev/mmcblk2p2 / ext4 defaults 0 1" > /mnt/mmc/etc/fstab
     umount /dev/mmcblk2
     echo Running fsck
     e2fsck -p -f /dev/mmcblk2p2
