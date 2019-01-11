d9ce83f43c1a887b8ac46e24f4d397583824b48a
Hal Emmerich <hal@halemmerich.com>
2019-01-11 11:41:56 -0600

    have user set timezone at install, issue #72

--- scripts/InstallScripts/InstallPackages.sh
+++ scripts/InstallScripts/InstallPackages.sh
@@ -27,6 +27,9 @@ while true; do
     esac
 done
 
+#Set the timezone
+dpkg-reconfigure tzdata
+
 #Install shared packages
 DEBIAN_FRONTEND=noninteractive apt install -y xorg acpi-support lightdm tasksel dpkg librsvg2-common xorg xserver-xorg-input-libinput alsa-utils anacron avahi-daemon eject iw libnss-mdns xdg-utils mousepad vlc dconf-cli dconf-editor sudo dtrx emacs25
 DEBIAN_FRONTEND=noninteractive apt install -y network-manager-gnome network-manager-openvpn network-manager-openvpn-gnome
