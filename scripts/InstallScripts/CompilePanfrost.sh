a8fb0d6681cf768345a9e4fb50e9484bae3cfc0b
rk-zero <r@k-ze.ro>
2019-10-02 16:49:59 +0200

    CompilePanfrost.sh with sudo

--- scripts/InstallScripts/CompilePanfrost.sh
+++ scripts/InstallScripts/CompilePanfrost.sh
@@ -1,15 +1,18 @@
-su -
+#!/bin/bash
 
-apt -y build-dep mesa
-apt -y install mesa-utils
+sudo apt -y build-dep mesa
+sudo apt -y install mesa-utils
+cd /tmp
 git clone https://gitlab.freedesktop.org/mesa/mesa -b master
 cd mesa
 mkdir build
 cd build
 meson .. . -Ddri-drivers= -Dvulkan-drivers= -Dgallium-drivers=panfrost,kmsro -Dlibunwind=false
-ninja install
+sudo ninja install
 
-apt -y remove llvm-7-dev
-apt -y autoremove
+sudo apt -y remove llvm-7-dev
+sudo apt -y autoremove
 
-reboot
+
+
+sudo reboot
