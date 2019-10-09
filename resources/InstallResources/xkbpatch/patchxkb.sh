6cdba6aeac9a823fc03bbabbe7e8de49c8d25b6b
rk-zero <r@k-ze.ro>
2019-10-09 18:00:51 +0200

    Patch for XKB Layouts for all Chromebooks.

---
+++ resources/InstallResources/xkbpatch/patchxkb.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cp ./compat/* /usr/share/X11/xkb/compat/
+cp ./keycodes/* /usr/share/X11/xkb/keycodes/
+cp ./symbols/* /usr/share/X11/xkb/symbols/
+
+patch /usr/share/X11/xkb/rules/base < ./rules/base.chromebook.patch
+patch /usr/share/X11/xkb/rules/base.lst < ./rules/base.lst.chromebook.patch
+patch /usr/share/X11/xkb/rules/base.xml < ./rules/base.xml.chromebook.patch
+patch /usr/share/X11/xkb/rules/evdev < ./rules/evdev.chromebook.patch
+patch /usr/share/X11/xkb/rules/evdev.lst < ./rules/evdev.lst.chromebook.patch
+patch /usr/share/X11/xkb/rules/evdev.xml < ./rules/evdev.xml.chromebook.patch
+
