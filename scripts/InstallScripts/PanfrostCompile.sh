4e65eabb9c1636316f3a2fd522072492d8d8acd2
rk-zero <r@k-ze.ro>
2019-10-23 08:46:18 +0200

    Panfrost Compile Script for PR

---
+++ scripts/InstallScripts/PanfrostCompile.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt -y install git libx11-dev meson pkg-config python3-setuptools python3-mako zlib1g-dev libexpat1-dev libdrm-dev bison flex libwayland-dev wayland-protocols libwayland-egl-backend-dev libxext-dev libxdamage-dev libx11-xcb-dev libxcb-glx0-dev libxcb-dri2-0-dev libxcb-dri3-dev libxcb-present-dev libxshmfence-dev libxxf86vm-dev libxrandr-dev gettext
+sudo apt -y install mesa-utils
+
+cd /tmp
+git clone https://gitlab.freedesktop.org/mesa/mesa -b "19.2"
+cd mesa
+mkdir build
+cd build
+meson .. . -Ddri-drivers= -Dvulkan-drivers= -Dgallium-drivers=panfrost,kmsro -Dlibunwind=false
+sudo ninja install
+
+echo "You may now reboot"
