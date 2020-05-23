2a456e35cf46d2ad1c34ac97bd9b60e7ae3062fe
Austin English <austinenglish@gmail.com>
2020-05-23 03:41:03 -0500

    scripts/InstallScripts/InstallPackages.sh,scripts/buildFilesystem.sh: install chromium

--- scripts/InstallScripts/InstallPackages.sh
+++ scripts/InstallScripts/InstallPackages.sh
@@ -37,7 +37,9 @@ dpkg-reconfigure tzdata
 DEBIAN_FRONTEND=noninteractive apt install -y xorg acpi-support lightdm tasksel dpkg librsvg2-common xorg xserver-xorg-input-libinput alsa-utils anacron avahi-daemon eject iw libnss-mdns xdg-utils mousepad vlc dconf-cli dconf-editor sudo dtrx emacs sysfsutils bluetooth
 DEBIAN_FRONTEND=noninteractive apt install -y network-manager-gnome network-manager-openvpn network-manager-openvpn-gnome
 
+# Browsers
 DEBIAN_FRONTEND=noninteractive apt install -y firefox-esr
+DEBIAN_FRONTEND=noninteractive apt install -y chromium
 
 [ "$DE" = "xfce" ] && apt install -y xfce4 dbus-user-session system-config-printer tango-icon-theme xfce4-power-manager xfce4-terminal xfce4-goodies numix-gtk-theme plank accountsservice papirus-icon-theme
 [ "$DE" = "lxqt" ] && apt install -y lxqt pavucontrol-qt
