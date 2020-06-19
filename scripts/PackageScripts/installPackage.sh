b78ab02f7b79af8c1604ec41f4fb67eba27e9c39
Hal Emmerich <hal@halemmerich.com>
2020-06-19 16:07:33 -0500

    change new scripts to use https links to Prawnos.com

--- scripts/PackageScripts/installPackage.sh
+++ scripts/PackageScripts/installPackage.sh
@@ -5,7 +5,7 @@ set -e
 # install the target package into the target location
 
 
-# This file is part of PrawnOS (http://www.prawnos.com)
+# This file is part of PrawnOS (https://www.prawnos.com)
 # Copyright (c) 2018 Hal Emmerich <hal@halemmerich.com>
 
 # PrawnOS is free software: you can redistribute it and/or modify
--- scripts/PackageScripts/uploadPackage.sh
+++ scripts/PackageScripts/uploadPackage.sh
@@ -5,7 +5,7 @@ set -e
 # upload the target package to the official prawnos apt repo
 # (of course this will only work if you can authenticate with the repo)
 
-# This file is part of PrawnOS (http://www.prawnos.com)
+# This file is part of PrawnOS (https://www.prawnos.com)
 # Copyright (c) 2018 Hal Emmerich <hal@halemmerich.com>
 
 # PrawnOS is free software: you can redistribute it and/or modify
