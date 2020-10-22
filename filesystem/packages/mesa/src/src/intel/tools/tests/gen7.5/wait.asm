4f18a24a847119307de523d2f44d8bbcda303fe1
SolidHal <hal@halemmerich.com>
2020-10-22 14:06:32 -0700

    Revert "bring mesa unstable into bullseye"
    This reverts commit 42afc306b01f8730f678e9360921eaf5577ffed8.

--- filesystem/packages/mesa/src/src/intel/tools/tests/gen7.5/wait.asm
+++
@@ -1,3 +0,0 @@
-wait(1)                         n0<0>.xUD                       { align16 WE_all 1N };
-wait(1)                         n0<0>.yUD                       { align16 WE_all 1N };
-wait(1)                         n0<0>.zUD                       { align16 WE_all 1N };
--- filesystem/packages/mesa/src/src/intel/tools/tests/gen8/cont.asm
+++
@@ -1,2 +0,0 @@
-cont(8)         JIP: 16         UIP: 64                         { align1 1Q };
-cont(16)        JIP: 16         UIP: 64                         { align1 1H };
