c6736948b0a650184eccc64f0ab5af4354e59bf7
SolidHal <hal@halemmerich.com>
2022-12-01 13:48:51 -0800

    improve ssh encrypted root handling

--- initramfs/resources/decrypt_root.sh
+++ initramfs/resources/decrypt_root.sh
@@ -3,16 +3,13 @@
 # CRYPTO_ROOTFS is set in /root/.profile by init when using ssh
 # or exported by init when this script is ran over serial console
 
+echo "Available devices:"
+blkid
+
+echo "================================================================================"
+
 echo "Opening encrypted root partition ${CRYPTO_ROOTFS}, this will take 30s..."
 cryptsetup --tries 5 luksOpen ${CRYPTO_ROOTFS} luksroot
 
-dmesg -n 7
-mount /dev/mapper/luksroot /newroot
+echo "This shell should die momentarily..."
 
-# /dev/pts may not be mounted
-umount /dev/pts || true
-umount /sys
-umount /proc
-
-#switch to the new rootfs
-exec switch_root /newroot /sbin/init
