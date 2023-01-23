fa6bcb1d9d8a20fee266aaa3b31632e8e2701210
SolidHal <eva@evaemmerich.com>
2023-01-22 17:58:08 -0800

    bring back arm64 drm patches that were accidentally removed

--- scripts/BuildScripts/KernelScripts/patchKernel.sh
+++ scripts/BuildScripts/KernelScripts/patchKernel.sh
@@ -41,11 +41,7 @@ if [ "$TARGET" == "$ARCH_ARMHF" ]; then
     for i in "$PATCHES"/DTS/*.patch; do echo $i; patch -p1 < $i; done
     for i in "$PATCHES"/kernel/*.patch; do echo $i; patch -p1 < $i; done
 elif [ "$TARGET" == "$ARCH_ARM64" ]; then
-    echo skip for now
-    # for i in "$PATCHES"/kernel/*.patch; do echo $i; patch -p1 < $i; done
-    # for i in "$PATCHES"/drm/*.patch; do echo $i; patch -p1 < $i; done
-    # for i in "$PATCHES"/cros-drm/*.patch; do echo $i; patch -p1 < $i; done
-    # for i in "$PATCHES"/alarm/*.patch; do echo $i; patch -p1 < $i; done
+    for i in "$PATCHES"/drm/*.patch; do echo $i; patch -p1 < $i; done
 elif [ "$TARGET" == "${ARCH_ARM64}-rk3588-server" ]; then
     echo skip for now, we are just using a git repo for the source
 else
